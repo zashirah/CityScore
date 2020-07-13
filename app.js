360f088f5337ed0e3e3980762af5000b1c004b86
Zach Shirah <zachshirah@MacBook-Pro.attlocal.net>
2020-07-13 18:51:48 -0400

    created app.js file and started connecting the app and api; also created a submit button to update the city list based on continent

---
+++ app.js
@@ -0,0 +1,20 @@
+// GET selection from Continent Dropdown on Updated button
+
+
+// GET Urban Areas List based on Continent dropdown
+
+
+
+
+async function getUrbanAreaList() {
+     const url = 'https://api.teleport.org/api/urban_areas';
+     try {
+          const response = await axios.get(url);
+          data = response.data._links['ua:item'];
+          console.log(data);
+          return data
+     } catch (error) {
+          console.log(error);
+     }
+};
+getUrbanAreaList();
