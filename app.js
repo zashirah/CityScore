29531d1ae7350095080077bac8124ca9cba8d3e6
Zach Shirah <zachshirah@MacBook-Pro.attlocal.net>
2020-07-13 19:17:26 -0400

    added event listener on dropdown to submit on enter. it's wonky though. will play with in the future if there is time

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ submitContinentButton.addEventListener('click', (e) => {
 });
 
 
+//  WORKING - want to add section for using "enter" on the list to submit the change
+
+// const continentDropdown = document.querySelector('#continents');
+
+// continentDropdown.addEventListener('keyup', (e) => {
+//      if (e.keyCode === 13) {
+//           e.preventDefault();
+//           const submittedContinent = document.querySelector('#continents').value
+//           console.log(submittedContinent);
+//      }
+// })
 
 
 // GET Urban Areas List based on Continent dropdown
